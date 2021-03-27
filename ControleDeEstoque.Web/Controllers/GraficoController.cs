81312b67c6e1712f9e32fb54510e322251c1f7ae
Otavio <luiz_otavio71@hotmail.com>
2021-03-27 10:41:39 -0300

    Foi adicionado os controllers e os nomes referente a barra de status

---
+++ ControleDeEstoque.Web/Controllers/GraficoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControleDeEstoque.Web.Controllers
+{
+    public class GraficoController : Controller
+    {
+        // GET: Grafico
+        public ActionResult PerdaMes()
+        {
+            return View();
+        }
+        public ActionResult EntradaSaídaMes()
+        {
+            return View();
+        }
+    }
+}
